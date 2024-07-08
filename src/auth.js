a446395467e5ec44c2e10c41da0b42d4195e131b
Hedrei Andrade <hedreiandrade@gmail.com>
2024-07-08 17:17:39 -0300

    token

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import {api} from './services/api';
 
-const isAuth = () => {
+const isAuth = async () => {
     if(localStorage.getItem('login_token') !== null){
-        return true;
+            const param = {token: localStorage.getItem('login_token')};
+            const response = await api.post('/verifyTokenRedirect', param);
+            if (response.data.status === 200){
+                return true;
+            }else{
+                return false;
+            }
+    }else{
+        return false;
     }
-    return false;
 };
 
 const PrivateRoute = ({component: Component, ...rest}) => {

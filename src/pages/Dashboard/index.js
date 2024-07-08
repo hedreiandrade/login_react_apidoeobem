e3f73c259f7ae54fcdfb2b2772ae6565517f9fb7
Hedrei Andrade <hedreiandrade@gmail.com>
2024-07-08 15:45:02 -0300

    redirect

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import { Link } from 'react-router-dom';
 import {useExpireToken} from "../../hooks/expireToken";
 
 export default function Dashboard() {
     useExpireToken();
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+
+    if(loading){
+        return '';
+    }
 
     return (
         <div>

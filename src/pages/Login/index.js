968fcbd837a0066fc1e51bd212d31c347b691b38
Hedrei Andrade <hedreiandrade@gmail.com>
2025-02-12 14:36:27 -0300

    Requied

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,41 +4,90 @@ import Header from '../../components/Header';
 import { api } from '../../services/api';
 
 export default class Login extends Component {
-
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             message: this.props.location.state ? this.props.location.state.message : '',
             isRegistering: false,
+            errors: {},
+            formData: {
+                name: '',
+                email: '',
+                password: '',
+                passwordConfirm: ''
+            }
         };
     }
 
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState((prevState) => ({
+            formData: {
+                ...prevState.formData,
+                [name]: value
+            },
+            errors: {
+                ...prevState.errors,
+                [name]: ''
+            }
+        }));
+    };
+
+    validateFields = () => {
+        let errors = {};
+        let isValid = true;
+        const { name, email, password, passwordConfirm } = this.state.formData;
+
+        if (!email) {
+            errors.email = "Field is required";
+            isValid = false;
+        }
+        if (!password) {
+            errors.password = "Field is required";
+            isValid = false;
+        }
+        if (this.state.isRegistering) {
+            if (!name) {
+                errors.name = "Field is required";
+                isValid = false;
+            }
+            if (!passwordConfirm) {
+                errors.passwordConfirm = "Field is required";
+                isValid = false;
+            }
+        }
+
+        this.setState({ errors });
+        return isValid;
+    };
+
     signIn = async () => {
-        const data = { email: this.email, password: this.password };
-        const response = await api.post('/login', data);
+        if (!this.validateFields()) return;
+
+        const { email, password } = this.state.formData;
+        const response = await api.post('/login', { email, password });
         if (typeof response.data.response.token === "undefined") {
             this.setState({ message: response.data.response });
         } else {
             localStorage.setItem('login_token', response.data.response.token);
             this.setState({ message: '' });
             this.props.history.push("/admin");
-            return;
         }
-    }
+    };
 
     createAccount = async () => {
-        const data = { name: this.name, email: this.email, password: this.password };
-        const response = await api.post("/user", data);
+        if (!this.validateFields()) return;
+
+        const { name, email, password } = this.state.formData;
+        const response = await api.post("/user", { name, email, password });
         if (response.status === 201) {
-            const dataLogin = { email: this.email, password: this.password };
-            const responseLogin = await api.post('/login', dataLogin);
+            const responseLogin = await api.post('/login', { email, password });
             if (typeof responseLogin.data.response.token === "undefined") {
                 this.setState({ message: 'Erro ao gerar token após registrar.' });
-            }else{
+            } else {
                 localStorage.setItem('login_token', responseLogin.data.response.token);
                 this.setState({ message: '' });
                 this.props.history.push("/admin");
-                return;
             }
         } else {
             this.setState({ message: 'Erro ao registrar a conta.' });
@@ -46,7 +95,7 @@ export default class Login extends Component {
     };
 
     toggleForm = () => {
-        this.setState({ isRegistering: !this.state.isRegistering, message: "" });
+        this.setState({ isRegistering: !this.state.isRegistering, message: "", errors: {} });
     };
 
     render() {
@@ -54,56 +103,42 @@ export default class Login extends Component {
             <div className="col-md-6">
                 <Header title={this.state.isRegistering ? "Register" : "ReactJS Login"} />
                 <hr className="my-3" />
-                {this.state.message !== "" ? <Alert color="danger" className="text-center">{this.state.message}</Alert> : ""}
+                {this.state.message && <Alert color="danger" className="text-center">{this.state.message}</Alert>}
 
-                {this.state.isRegistering ? (
-                    <Form>
+                <Form>
+                    {this.state.isRegistering && (
                         <FormGroup>
                             <Label for="name">Name</Label>
-                            <Input type="text" id="name" name="name" onChange={(e) => (this.name = e.target.value)} placeholder="Type your name" />
+                            <Input type="text" id="name" name="name" value={this.state.formData.name} onChange={this.handleChange} placeholder="Type your name" />
+                            {this.state.errors.name && <Label className="text-danger">{this.state.errors.name}</Label>}
                         </FormGroup>
+                    )}
+                    <FormGroup>
+                        <Label for="email">Email</Label>
+                        <Input type="email" id="email" name="email" value={this.state.formData.email} onChange={this.handleChange} placeholder="Type your e-mail" />
+                        {this.state.errors.email && <Label className="text-danger">{this.state.errors.email}</Label>}
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password">Password</Label>
+                        <Input type="password" id="password" name="password" value={this.state.formData.password} onChange={this.handleChange} placeholder="Type your password" />
+                        {this.state.errors.password && <Label className="text-danger">{this.state.errors.password}</Label>}
+                    </FormGroup>
+                    {this.state.isRegistering && (
                         <FormGroup>
-                            <Label for="email">Email</Label>
-                            <Input type="email" id="email" name="email" onChange={(e) => (this.email = e.target.value)} placeholder="Type your e-mail" />
+                            <Label for="passwordConfirm">Confirm Password</Label>
+                            <Input type="password" id="passwordConfirm" name="passwordConfirm" value={this.state.formData.passwordConfirm} onChange={this.handleChange} placeholder="Confirm your password" />
+                            {this.state.errors.passwordConfirm && <Label className="text-danger">{this.state.errors.passwordConfirm}</Label>}
                         </FormGroup>
-                        <FormGroup>
-                            <Label for="password">Password</Label>
-                            <Input type="password" id="password" name="password" onChange={(e) => (this.password = e.target.value)} placeholder="Type your password" />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="password">Confirm Password</Label>
-                            <Input type="password" id="password_confirme" name="password" onChange={(e) => (this.password = e.target.value)} placeholder="Type your password" />
-                        </FormGroup>
-                        <div className="button-container">
-                            <Button color="primary" className="align-button" onClick={this.createAccount}>
-                                Register
-                            </Button>
-                            <Button color="secondary" className="align-button" onClick={this.toggleForm}>
-                                Back to Login
-                            </Button>
-                        </div>
-                    </Form>
-                ) : (
-                    // Formulário de Login
-                    <Form>
-                        <FormGroup>
-                            <Label for="email">Email</Label>
-                            <Input type="text" id="email" name="email" onChange={(e) => (this.email = e.target.value)} placeholder="Type your e-mail" />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="password">Password</Label>
-                            <Input type="password" id="password" name="password" onChange={(e) => (this.password = e.target.value)} placeholder="Type your password" />
-                        </FormGroup>
-                        <div className="button-container">
-                            <Button color="primary" className="align-button" onClick={this.signIn}>
-                                Sign In
-                            </Button>
-                            <Button color="primary" className="align-button" onClick={this.toggleForm}>
-                                Register Account
-                            </Button>
-                        </div>
-                    </Form>
-                )}
+                    )}
+                    <div className="button-container">
+                        <Button color="primary" className="align-button" onClick={this.state.isRegistering ? this.createAccount : this.signIn}>
+                            {this.state.isRegistering ? "Register" : "Sign In"}
+                        </Button>
+                        <Button color="secondary" className="align-button" onClick={this.toggleForm}>
+                            {this.state.isRegistering ? "Back to Login" : "Register Account"}
+                        </Button>
+                    </div>
+                </Form>
             </div>
         );
     }

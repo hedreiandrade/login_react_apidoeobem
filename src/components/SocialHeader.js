3364f04c69823a5126eaa4255702771bfcd855bc
Hedrei Andrade <hedreiandrade@gmail.com>
2025-03-01 12:08:01 -0300

    hotfix/photo

--- src/components/SocialHeader.js
+++ src/components/SocialHeader.js
@@ -8,8 +8,8 @@ export default function SocialHeader({ user }) {
             <h3>Social Media</h3>
             <div style={{ display: "flex", alignItems: "center" }}>
                 <img src={user.photo} alt="User" style={{ width: "40px", height: "40px", borderRadius: "50%", marginRight: "10px" }} />
-                <Link to="/edit-profile" style={{ color: "#fff", textDecoration: "none" }}>Editar Perfil</Link>
-                <Link to="/logout" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>LogOut</Link>
+                <Link to="/edit-profile" style={{ color: "#fff", textDecoration: "none" }}>Edit profile</Link>
+                <Link to="/logout" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>Log out</Link>
             </div>
         </header>
     );

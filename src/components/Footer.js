b40cfed273738b60ee5ad353dc432363d7a11ded
Hedrei Andrade <hedreiandrade@gmail.com>
2025-03-01 12:10:30 -0300

    Description

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer() {
     return (
         <footer style={{ textAlign: "center", padding: "10px", background: "#f8f9fa", marginTop: "20px" }}>
-            <p>&copy; 2025 - Minha Aplicação</p>
+            <p>&copy; 2025 - Hedrei Andrade</p>
         </footer>
     );
 }

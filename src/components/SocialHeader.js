91b4d88c2d346843dda4afab2dfeb1a88b4d4f1b
Hedrei Andrade <hedreiandrade@gmail.com>
2025-03-08 22:16:29 -0300

    barra

--- src/components/SocialHeader.js
+++ src/components/SocialHeader.js
@@ -5,11 +5,12 @@ import { Link } from "react-router-dom";
 export default function SocialHeader({ user }) {
     return (
         <header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px", background: "#007bff", color: "#fff" }}>
-            <h3>Social Media</h3>
+            <Link to="/admin" style={{ color: "#fff", textDecoration: "none" }}><h4>Social Media</h4></Link>
             <div style={{ display: "flex", alignItems: "center" }}>
                 <img src={user.photo} alt="User" style={{ width: "40px", height: "40px", borderRadius: "50%", marginRight: "10px" }} />
-                <Link to="/edit-profile" style={{ color: "#fff", textDecoration: "none" }}>Edit profile</Link>
-                <Link to="/logout" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>Log out</Link>
+                <Link to="/edit-profile" style={{ color: "#fff", textDecoration: "none" }}>Edit</Link>
+                <Link to="/followers" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>3M Followers</Link>
+                <Link to="/logout" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>Logout</Link>
             </div>
         </header>
     );

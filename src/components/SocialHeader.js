c0d133823e14626aff280d56a7923a7249ee737f
Hedrei Andrade <hedreiandrade@gmail.com>
2025-03-08 22:17:58 -0300

    barra

--- src/components/SocialHeader.js
+++ src/components/SocialHeader.js
@@ -7,9 +7,9 @@ export default function SocialHeader({ user }) {
         <header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px", background: "#007bff", color: "#fff" }}>
             <Link to="/admin" style={{ color: "#fff", textDecoration: "none" }}><h4>Social Media</h4></Link>
             <div style={{ display: "flex", alignItems: "center" }}>
+                <Link to="/followers" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>3M Followers</Link>
                 <img src={user.photo} alt="User" style={{ width: "40px", height: "40px", borderRadius: "50%", marginRight: "10px" }} />
                 <Link to="/edit-profile" style={{ color: "#fff", textDecoration: "none" }}>Edit</Link>
-                <Link to="/followers" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>3M Followers</Link>
                 <Link to="/logout" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>Logout</Link>
             </div>
         </header>

e93428f7641cceb087b64ae3a1468b8ee87eb6e7
Hedrei Andrade <hedreiandrade@gmail.com>
2025-03-08 22:21:48 -0300

    barra

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({title}) => (
 	<header>
-		<h1 className="text-center font-weight-bold">{title}</h1>
+		<h2 className="text-center font-weight-bold">{title}</h2>
 	</header>
 );
 
--- src/components/SocialHeader.js
+++ src/components/SocialHeader.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function SocialHeader({ user }) {
     return (
         <header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px", background: "#007bff", color: "#fff" }}>
-            <Link to="/admin" style={{ color: "#fff", textDecoration: "none" }}><h4>Social Media</h4></Link>
+            <Link to="/admin" style={{ color: "#fff", textDecoration: "none" }}><h4>H Media</h4></Link>
             <div style={{ display: "flex", alignItems: "center" }}>
                 <Link to="/followers" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>3M Followers</Link>
                 <img src={user.photo} alt="User" style={{ width: "40px", height: "40px", borderRadius: "50%", marginRight: "10px" }} />

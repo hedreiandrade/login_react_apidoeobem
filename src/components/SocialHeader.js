b665c8946de2108a836c477b809cfc9a7994ad74
Hedrei Andrade <hedreiandrade@gmail.com>
2025-04-26 14:13:29 -0300

    Adicionando número de seguidores

--- src/components/SocialHeader.js
+++ src/components/SocialHeader.js
@@ -1,15 +1,40 @@
 // src/components/SocialHeader.js
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { apiFeed } from "../services/api";
 
 export default function SocialHeader({ user }) {
+    const [followersCount, setFollowersCount] = useState(0);
+
+    useEffect(() => {
+        async function fetchFollowersCount() {
+            try {
+                const userId = localStorage.getItem('user_id');
+                const token = localStorage.getItem('login_token');
+                const response = await apiFeed.get(`/followers/${userId}/1/5`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                if (response.data && typeof response.data.total === 'number') {
+                    setFollowersCount(response.data.total);
+                }
+            } catch (error) {
+                console.error('Failed to fetch followers count', error);
+            }
+        }
+        fetchFollowersCount();
+    }, []);
+
     return (
         <header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px", background: "#007bff", color: "#fff" }}>
             <Link to="/admin" style={{ color: "#fff", textDecoration: "none" }}><h4>H Media</h4></Link>
             <div style={{ display: "flex", alignItems: "center" }}>
-                <Link to="/followers" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>3M Followers</Link>
-                <img src={user.photo} alt="User" style={{ width: "40px", height: "40px", borderRadius: "50%", marginRight: "10px" }} />
-                <Link to="/edit-profile" style={{ color: "#fff", textDecoration: "none" }}>Edit</Link>
+                <Link to="/followers" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>
+                    {followersCount} Followers
+                </Link>
+                <img src={user.photo} alt="User" style={{ width: "40px", height: "40px", borderRadius: "50%", margin: "0 10px" }} />
+                <Link to="/edit-profile" style={{ color: "#fff", textDecoration: "none", marginRight: "10px" }}>Edit</Link>
                 <Link to="/logout" className="btn btn-outline-primary" style={{ color: "#fff", textDecoration: "none" }}>Logout</Link>
             </div>
         </header>

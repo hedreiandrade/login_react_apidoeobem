8ee5a33c375aafce46fed1e000a091c4d5062777
Hedrei Andrade <hedreiandrade@gmail.com>
2025-05-01 01:45:39 -0300

    Melhorias

--- src/hooks/userToken.js
+++ src/hooks/userToken.js
@@ -1,4 +1,11 @@
+
+import {useHistory} from "react-router-dom";
+
 function useToken() {
+    const history = useHistory();
+    if(localStorage.getItem('login_token') === null || localStorage.getItem('login_token') === ''){
+        history.push("/");
+    }
     const token = localStorage.getItem('login_token');
     return token;
 }

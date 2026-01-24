be03bb4d0ae7f49fa7256a2c111a20034e99c53a
Hedrei Andrade <hedreiandrade@gmail.com>
2026-01-24 19:32:28 -0300

    Permitindo apenas usuario deletar respot

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1046,7 +1046,7 @@ export default function ProfilePage() {
                                         </div>
                                         
                                         {/* Botão de deletar disponível para posts do usuário (originais e reposts) */}
-                                        {(isPostOwner || (post.is_repost && post.original_user_id === userId)) ? (
+                                        {(isPostOwner || (post.is_repost && post.user_id === userId)) ? (
                                             <Button 
                                                 color="link" 
                                                 size="sm"
